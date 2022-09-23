da0e3dcb716e0da95b5f100066ea3192da247e74
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2022-09-23 17:23:40 +0200

    money time

---
+++ src/main/java/com/brief/ionicBack/config/InitDatabase.java
@@ -0,0 +1,24 @@
+package com.brief.ionicBack.config;
+
+import com.brief.ionicBack.model.ERole;
+import com.brief.ionicBack.model.Role;
+import com.brief.ionicBack.repository.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class InitDatabase implements CommandLineRunner {
+
+    @Autowired
+    private RoleRepository repository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Role role = new Role(null, ERole.ROLE_ADMIN);
+        repository.save(role);
+        System.out.println("role sauvé");
+
+    }
+}
