25cb681c760d6e802b5cc1177d55b444a90444db
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2022-10-12 15:26:56 +0200

    Role update 5

--- src/main/java/com/brief/ionicBack/config/InitDatabase.java
+++ src/main/java/com/brief/ionicBack/config/InitDatabase.java
@@ -16,11 +16,11 @@ public class InitDatabase implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        /*if (repository.findAll().size() >= 1) {
+        if (repository.findAll().size() >= 0) {
             Role role = new Role(null, ERole.ROLE_ADMIN);
             repository.save(role);
             System.out.println("if role sauvé");
-        }*/
+        }
         System.out.println("role sauvé");
 
     }
